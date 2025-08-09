d36e1206c821f61944e518102b97b9ed1a098bda
Anthony Carapetis <anthony.carapetis@gmail.com>
2025-08-09 14:01:55 +1000

    Get LSPs working again

--- ftdetect/jjconfig.lua
+++ ftdetect/jjconfig.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   pattern = "*.toml",
   callback = function()
     if vim.fn.getline(1) == '"$schema" = "https://jj-vcs.github.io/jj/latest/config-schema.json"' then
-      vim.bo.filetype = 'jjconfig'
+      vim.bo.filetype = 'jjconfig.toml'
     end
   end
 })
