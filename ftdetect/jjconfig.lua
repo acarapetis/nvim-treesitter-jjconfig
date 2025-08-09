a9d9e044e7a343ff701715da63f7c01e9616f605
Anton Älgmyr <anton@algmyr.se>
2025-08-09 16:47:39 +0200

    Apply autoformatting to all lua files

--- ftdetect/jjconfig.lua
+++ ftdetect/jjconfig.lua
@@ -1,8 +1,8 @@
-vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-  pattern = "*.toml",
-  callback = function()
-    if vim.fn.getline(1) == '"$schema" = "https://jj-vcs.github.io/jj/latest/config-schema.json"' then
-      vim.bo.filetype = 'jjconfig.toml'
-    end
-  end
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = "*.toml",
+    callback = function()
+        if vim.fn.getline(1) == '"$schema" = "https://jj-vcs.github.io/jj/latest/config-schema.json"' then
+            vim.bo.filetype = "jjconfig.toml"
+        end
+    end,
 })
