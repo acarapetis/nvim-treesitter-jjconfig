c8477f1824f0abd72fc94cec19989b11ab24be22
Anthony Carapetis <anthony.carapetis@gmail.com>
2025-12-05 09:42:10 +1100

    Support Taplo's #:schema directive
    Resolves #3

--- ftdetect/jjconfig.lua
+++ ftdetect/jjconfig.lua
@@ -1,8 +1,11 @@
-local jj_schema_re = vim.regex([["$schema" = "https://jj-vcs\.github\.io/jj/.*/config-schema\.json"]])
+local json_schema_re =
+    vim.regex([["$schema" = "https://\(jj-vcs\.github\.io/jj\|docs.jj-vcs.dev\)/.*/config-schema\.json"]])
+local taplo_comment_re =
+    vim.regex([[^#:schema https://\(jj-vcs\.github\.io/jj\|docs.jj-vcs.dev\)/.*/config-schema\.json]])
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     pattern = "*.toml",
     callback = function()
-        if jj_schema_re:match_line(0, 0) then vim.bo.filetype = "jjconfig.toml" end
+        if json_schema_re:match_line(0, 0) or taplo_comment_re:match_line(0, 0) then vim.bo.filetype = "jjconfig.toml" end
     end,
 })
